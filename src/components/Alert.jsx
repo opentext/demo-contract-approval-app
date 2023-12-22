5a39105e32dfafbe750ea6417ab83267ef5f49ab
jpluimer <jpluimer@opentext.com>
2023-12-22 15:43:20 +0100

    Fixed Alert in Snackbar issue

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,8 @@
-import MuiAlert from '@mui/material/Alert';
+import React from 'react';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
-function Alert(props) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default Alert;
